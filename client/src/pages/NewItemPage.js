7a8b309dc0c905be2c30edc508236a7e9b2cf229
Bassem Mackieh <bmackieh@gmail.com>
2020-09-28 21:20:02 +0300

    first commit

--- client/src/pages/NewItemPage.js
+++ client/src/pages/NewItemPage.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Auth } from 'aws-amplify';
 import NewItem from '../components/NewItem';
 
 const useStyles = makeStyles({
@@ -16,12 +17,37 @@ const useStyles = makeStyles({
 });
 
 const NewServicePage = () => {
+  const [isLoggedIn, setLoginStatus] = useState(false);
   const classes = useStyles();
-  return (
-    <div className={classes.root} data-testid="newServicePage">
-      <NewItem />
-    </div>
-  );
+  const checkUserAuthentication = async () => {
+    await Auth.currentAuthenticatedUser()
+      .then((user) => {
+        setLoginStatus(true);
+        console.log(user);
+        console.log('Should be good');
+      })
+      .catch((err) => {
+        setLoginStatus(false);
+        console.log(err);
+        console.log('Should not be good');
+      });
+  };
+  useEffect(() => {
+    checkUserAuthentication();
+  });
+  if (isLoggedIn) {
+    return (
+      <div className={classes.root} data-testid="newServicePage">
+        <NewItem />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>You are not authenticated.</h1>
+      </div>
+    );
+  }
 };
 
 export default NewServicePage;
